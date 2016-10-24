40e491a2eddbe496e61c9317f32a664ab22cb708
Vanessa Holland <vanessaholland@legalshieldcorp.com>
2016-10-24 12:25:53 -0500

    checkpoint-8 completed plus cubic-bezier animation

---
+++ bloc-jams/js/landing.js
@@ -0,0 +1,17 @@
+var animatePoints = function() {
+ 
+    var points = document.getElementsByClassName('point');
+    
+    var revealPoint = function(i) {
+         points[i].style.opacity = 1;
+         points[i].style.transform = "scaleX(1) translateY(0)";
+         points[i].style.msTransform = "scaleX(1) translateY(0)";
+         points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+    
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(i);
+    }
+    
+ };
+
