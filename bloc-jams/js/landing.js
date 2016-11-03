a41beeb6a9ed0210a72d517b6610b02d861d77fa
Vanessa Holland <vanessaholland@legalshieldcorp.com>
2016-11-03 09:55:59 -0500

    checkpoint-16 completed

--- bloc-jams/js/landing.js
+++ bloc-jams/js/landing.js
@@ -1,30 +1,24 @@
-var pointsArray = document.getElementsByClassName('point');
- 
-var revealPoint = function(point) {
-    point.style.opacity = 1;
-    point.style.transform = "scaleX(1) translateY(0)";
-    point.style.msTransform = "scaleX(1) translateY(0)";
-    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function() {
+   var revealPoint = function() {
+            // #7
+            $(this).css({
+                opacity: 1,
+                transform: 'scaleX(1) translateY(0)'
+            });
+    };
+    $.each($('.point'), revealPoint);
 };
 
- var animatePoints = function(points) {
-    forEach(points, revealPoint);
-    
- };
+ $(window).load(function() {
+   if ($(window).height() > 950) {
+      animatePoints();
+   }
+   
+   var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
- window.onload = function() {
-     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
-     }
-     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
-
-      window.addEventListener('scroll', function(event) {
-     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
+   $(window).scroll(function(event) {
+      if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
          }
-
      });
- }
-
+  });
